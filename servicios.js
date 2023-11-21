b14f3ef1fe894a8ed009652b7d423197a4cd916f
yennybejarano <yennybejarano937.com>
2023-11-21 17:47:15 -0500

    asd

---
+++ servicios.js
@@ -0,0 +1,3 @@
+// Este archivo puede ser utilizado para manipulación dinámica del cuadro comparativo si necesitas interactuar con los datos.
+// Por ejemplo, podrías agregar filas o cambiar valores en función de eventos del usuario.
+// En este ejemplo, no se necesita JavaScript para el cuadro comparativo estático.
